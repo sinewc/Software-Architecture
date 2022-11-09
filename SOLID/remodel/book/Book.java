f25c0417c2892eaaee22c375ffeb1799e618a6d0
sinewc <69142166+sinewc@users.noreply.github.com>
2022-11-09 22:28:28 +0700

    addSolid

---
+++ SOLID/remodel/book/Book.java
@@ -0,0 +1,45 @@
+// package com.solid.book;
+
+import java.util.List;
+import java.util.ListIterator;
+
+public class Book {
+    private String title;
+    private ListIterator<String> iterator;
+    private String currentPage;
+
+
+    public Book(String title, List<String> pages)  {
+        this.title = title;
+        iterator = pages.listIterator();
+        if (iterator.hasNext()) {
+            currentPage = iterator.next();
+        } else {
+            currentPage = null;
+        }
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCurrentPage() {
+        return currentPage;
+    }
+
+    public boolean turnToNextPage() {
+        if (iterator.hasNext()) {
+            currentPage = iterator.next();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public void print(Printer printer) {
+        printer.print(this);
+    }
+
+
+}
